07274b5ee06ac119500bafdcd6dd1f95d5c2b79c
Mengxia Yu <myu2@crcfe01.crc.nd.edu>
2022-08-24 11:46:57 -0400

    add parsing tool

---
+++ simple-wikidata-db/run.sh
@@ -0,0 +1,7 @@
+python preprocess_dump.py --input_file /Users/mengxiayu/Downloads/wikidata/wikidata-20220103-all.json.gz \
+--out_dir /Users/mengxiayu/Documents/Research/WIKIPEDIA/wikidata \
+--batch_size 400000 \
+--language_id en \
+--processes 4 \
+--num_lines_read 432243466 \
+--num_lines_in_dump 432243466 \
